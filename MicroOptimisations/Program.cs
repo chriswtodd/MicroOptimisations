832e602d7ff90bad6ec40ad80e938bd4cec17bb4
Chris Todd <chriswilltodd@gmail.com>
2025-05-22 21:07:12 +1200

    bug: update readme for CpuCaching

--- MicroOptimisations/Program.cs
+++ MicroOptimisations/Program.cs
@@ -1,10 +1,12 @@
 ﻿using BenchmarkDotNet.Running;
+using MicroOptimisations.CpuCaching;
 
 public class Program 
 {
     public static void Main(string[] args) 
     {
-        RunBenchmarks();
+        CpuInfo.GetCacheSizes().ForEach(d => Console.WriteLine(d));
+        //RunBenchmarks();
     }
 
     public static void RunBenchmarks()
@@ -12,7 +14,7 @@ public class Program
         var summaryForwarding = BenchmarkRunner.Run<MicroOptimisations.Forwarding.Bench>();
         var summaryReferencing = BenchmarkRunner.Run<MicroOptimisations.Referencing.Bench>();
         var summaryCacheLevelsBench = BenchmarkRunner.Run<MicroOptimisations.CpuCaching.CacheLevelsBench>();
-        var summaryCacheLineFalseSharing = BenchmarkRunner.Run<MicroOptimisations.CpuCaching.CacheLineFalseSharing>();
+        var summaryCacheLineFalseSharing = BenchmarkRunner.Run<MicroOptimisations.CpuCaching.CacheLineFalseSharingBench>();
         var summaryCacheLineUpdatingBench = BenchmarkRunner.Run<MicroOptimisations.CpuCaching.CacheLineUpdatingBench>();
         var summaryLoops = BenchmarkRunner.Run<MicroOptimisations.Loops.Bench>();
     }
